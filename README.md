97d598ed7dce3c1d243515c4d933395a8da0242c
JK Gunnink <jgunnink@gmail.com>
2021-06-29 00:28:44 +0800

    convert function to TS

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 This is a working repo to test a proof of concept. It's very much in pre-alpha. Nothing works, it's far from complete,
 do not use this.
+
+Function targets nodejs 14.
---
+++ functions/notify-github/src/index.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+export const notifyGithub = (req: any, res: any) => {
+  const r = req.body;
+  const sha = r.sha;
+  const repo = r.repo;
+  const owner = r.owner || "jgunnink";
+  const url = `https://api.github.com/repos/${owner}/${repo}/statuses/${sha}`;
+
+  const data = JSON.stringify({
+    state: r.state,
+    context: r.context,
+    description: r.description,
+    // target_url: target_url, Used when providing a link to the output.
+  });
+
+  const config = {
+    headers: {
+      Accept: "application/vnd.github.v3+json",
+      Authorization: `token ${process.env.GITHUB_TOKEN}`,
+      "Content-Type": "application/json",
+      "Content-Length": data.length,
+      "User-Agent": "jgunnink-workflow-bot",
+    },
+  };
+
+  axios
+    .post(url, data, config)
+    .then(res => {
+      console.log(`statusCode: ${res.status}`);
+      console.log(res);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+};
