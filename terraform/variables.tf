c32e10dd249f8e8e2f30c7b658faacb4d64e613e
JK Gunnink <jgunnink@gmail.com>
2021-07-29 23:28:38 +0800

    Make the workflow name a variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,19 @@
 variable "project_id" {
   description = "The project ID in Google Cloud to use for these resources."
-  default     = "cloud-pipeline-dev"
 }
 
 variable "region" {
   description = "The region in Google Cloud where the resources will be deployed."
-  default     = "australia-southeast1"
 }
 
 variable "repo_name" {
   description = "The name of the respository for cloudbuild to use with triggers"
-  default     = "workflow-cicd-demo"
 }
 
 variable "repo_owner" {
   description = "The name of the owner of the repo. For example, this could be GoogleCloudPlatform or jgunnink"
-  default     = "jgunnink"
+}
+
+variable "workflow_name" {
+  description = "The name of the workflow which will run the pipeline"
 }
--- terraform/workflow.tf
+++ terraform/workflow.tf
@@ -1,5 +1,5 @@
-resource "google_workflows_workflow" "workflow-2" {
-  name            = "workflow-2"
+resource "google_workflows_workflow" "workflow" {
+  name            = var.workflow_name
   region          = "asia-southeast1"
   description     = "CI/CD Pipeline"
   service_account = "projects/${var.project_id}/serviceAccounts/${google_service_account.workflow_runner_service_account.email}"
