30e35de138aa6ff1f24d315602cd7d80f2a75c1c
JK Gunnink <jgunnink@gmail.com>
2022-08-28 13:05:09 +0800

    add script and update diff cloudbuilder

--- functions/gitops/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-
-export const gitops = (req: Request, res: Response) => {
-  const replyMessage = req.body.message || "Hello World!";
-  const response = { message: replyMessage };
-
-  res.send(response);
-  return response; // Specify a return value here so we don't need to kick up an express-server for testing.
-};
