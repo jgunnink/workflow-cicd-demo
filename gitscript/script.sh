51f80d0b62392fb803c30e7a1366abc6b3d9598e
JK Gunnink <jgunnink@gmail.com>
2022-08-28 13:09:03 +0800

    remove opening declaration in script

--- gitscript/script.sh
+++ gitscript/script.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 if [ git checkout master && git pull origin master --depth=1 && git checkout $COMMIT_SHA && (git diff --quiet HEAD~1 $COMMIT_SHA ${_COMPARISON}) ]; then
   return "no_change"
 else
