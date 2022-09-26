dd8a081741400767f22b6db88c2015a9064a8fe0
JK Gunnink <jgunnink@gmail.com>
2022-09-26 19:49:00 +0800

    remove use of extra script for cleanliness

--- gitscript/script.sh
+++
@@ -1,9 +0,0 @@
-git checkout master && \
-git pull origin master --depth=1 && \
-git checkout $COMMIT_SHA
-
-if [ git diff --quiet HEAD~1 $COMMIT_SHA ${_COMPARISON} ]; then
-  return "no_change"
-else
-  return "changes"
-fi
